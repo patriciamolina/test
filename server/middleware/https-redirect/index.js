08c6f618e40044edd66264e78974230a18dac36e
Felipe Aguayo <faguayo@produccion.datactil.com>
2014-11-20 18:37:25 -0300

    Se configura todos los plugin

--- server/middleware/https-redirect/index.js
+++ server/middleware/https-redirect/index.js
@@ -4,14 +4,14 @@
  * @returns {Function} The express middleware handler
  */
 module.exports = function(options) {
-  options = options || {};
-  var httpsPort = options.httpsPort || 443;
-  return function(req, res, next) {
-    if (!req.secure) {
-      var parts = req.get('host').split(':');
-      var host = parts[0] || '127.0.0.1';
-      return res.redirect('https://' + host + ':' + httpsPort + req.url);
-    }
-    next();
-  };
+    options = options || {};
+    var httpsPort = options.httpsPort || 3001;
+    return function httpsRedirect(req, res, next) {
+        if (!req.secure) {
+            var parts = req.get('host').split(':');
+            var host = parts[0] || '127.0.0.1';
+            return res.redirect('https://' + host + ':' + httpsPort + req.url);
+        }
+        next();
+    };
 };
