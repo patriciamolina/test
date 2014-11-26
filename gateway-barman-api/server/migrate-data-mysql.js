0dad0161c3b7d0703781f582e7fc9403a68f54bd
Felipe Aguayo <faguayo@produccion.datactil.com>
2014-11-26 11:35:27 -0300

    Reestructuracion y separacion de proyectos

--- gateway-barman-api/server/migrate-data-mysql.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Felipe on 24-11-2014.
- */
-var server = require('./server');
-var dataSource = server.dataSources.INFOSESION;
-
-dataSource.automigrate(['User', 'Application', 'Role', 'ACL'], function(er) {
-    if (er) throw er;
-});
