5eaf000c87736b637e0d6d87ecb143502fad9040
Felipe Aguayo <faguayo@produccion.datactil.com>
2014-12-17 10:58:23 -0300

    prueba de solucion para No 'Access-Control-Allow-Origin'

--- server/server.js
+++ server/server.js
@@ -52,6 +52,26 @@ app.use(loopback.urlNotFound());
 // The ultimate error handler.
 app.use(loopback.errorHandler());
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', app.get('url'));
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.start = function() {
     Utils.alterModels(app);
     var port = app.get('port');
